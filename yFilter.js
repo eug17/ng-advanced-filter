d1108e5c888a5241426716501e17ab0bc82c1cfa
Yevhen Kotliar <eugen.kotliar@gmail.com>
2016-10-17 23:03:43 -0400

    Initial

---
+++ yFilter.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+
+    angular.module('yFilter')    
+    .filter('yFilter', function() {
+        return function(a, query, b) {
+            // a - innitial short array
+            // b - full list
+            var array;
+            if(b){
+                array = b;
+            }else{
+                array = a;
+            }
+            var newQuery = query && query.trim().split(/\s+/);
+            if(angular.isDefined(array) && array.length > 0){
+                var keys = Object.keys(array[0]);
+            }
+            
+            if(!newQuery || newQuery.length < 2) 
+                return a;
+            if(query.length < 3)
+                return a;
+            // in big arrays to start request after 3th character
+            return array.filter(function(obj) {
+                return newQuery.every(function(q) {
+                    return keys.some(function(key) {
+                        if(Array.isArray(obj[key])){
+                            return JSON.stringify(obj[key]).toLowerCase().indexOf(q.toLowerCase()) > -1;
+                        }else{
+                            return String(obj[key]).toLowerCase().indexOf(q.toLowerCase()) > -1;
+                        }
+                    });
+                });
+            });
+        };
+    });
+})();
